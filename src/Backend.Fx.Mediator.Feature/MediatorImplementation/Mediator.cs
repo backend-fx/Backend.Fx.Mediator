140e9e960d0a87fd0474b828a906bf1c0aea0443
Marc Wittke <marc@wittke-web.de>
2026-05-12 05:55:56 -0300

    fix(mediator): use correct identity when auto notifying

--- src/Backend.Fx.Mediator.Feature/MediatorImplementation/Mediator.cs
+++ src/Backend.Fx.Mediator.Feature/MediatorImplementation/Mediator.cs
@@ -13,7 +13,8 @@ internal sealed class Mediator : IMediator
     private readonly MediatorOptions _options;
     private readonly HandlerRegistry _handlerRegistry;
 
-    public Mediator(IRootMediator rootMediator, IServiceProvider serviceProvider, MediatorOptions options, HandlerRegistry handlerRegistry)
+    public Mediator(IRootMediator rootMediator, IServiceProvider serviceProvider, MediatorOptions options,
+        HandlerRegistry handlerRegistry)
     {
         _rootMediator = rootMediator;
         _serviceProvider = serviceProvider;
@@ -22,44 +23,66 @@ internal sealed class Mediator : IMediator
     }
 
 
-    public ValueTask NotifyAsync<TNotification>(TNotification notification, CancellationToken cancellation = default) where TNotification : class
+    public ValueTask NotifyAsync<TNotification>(TNotification notification, CancellationToken cancellation = default)
+        where TNotification : class
     {
         return _rootMediator.NotifyAsync(notification, cancellation);
     }
 
-    public ValueTask NotifyAsync<TNotification>(TNotification notification, IIdentity notifier,
+    public ValueTask NotifyAsync<TNotification>(
+        TNotification notification,
+        IIdentity notifier,
         CancellationToken cancellation = default) where TNotification : class
     {
         return _rootMediator.NotifyAsync(notification, notifier, cancellation);
     }
 
-    public ValueTask NotifyAsync<TNotification>(TNotification notification, INotificationErrorHandler errorHandler,
+    public ValueTask NotifyAsync<TNotification>(
+        TNotification notification,
+        INotificationErrorHandler errorHandler,
         CancellationToken cancellation = default) where TNotification : class
     {
         return _rootMediator.NotifyAsync(notification, errorHandler, cancellation);
     }
 
-    public ValueTask NotifyAsync<TNotification>(TNotification notification, IIdentity notifier,
+    public ValueTask NotifyAsync<TNotification>(
+        TNotification notification,
+        IIdentity notifier,
         INotificationErrorHandler errorHandler, CancellationToken cancellation = default) where TNotification : class
     {
         return _rootMediator.NotifyAsync(notification, notifier, errorHandler, cancellation);
     }
 
-    public ValueTask<TResponse> RequestAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellation = default) where TResponse : class
+    public async ValueTask<TResponse> RequestAsync<TResponse>(
+        IRequest<TResponse> request,
+        CancellationToken cancellation = default) where TResponse : class
     {
-        return _rootMediator.RequestAsync(request, cancellation);
+        TResponse response = await new RequestDispatch(_handlerRegistry, _serviceProvider)
+            .DispatchAsync(request, _options.DefaultRequestor, cancellation)
+            .ConfigureAwait(false);
+
+        if (_options.AutoNotifyResponses)
+        {
+            _logger.LogInformation("Sending response of type {Response} also as notification", typeof(TResponse).Name);
+            await NotifyAsync(response, _options.DefaultNotifier, cancellation).ConfigureAwait(false);
+        }
+
+        return response;
     }
 
-    public async ValueTask<TResponse> RequestAsync<TResponse>(IRequest<TResponse> request, IIdentity requestor, CancellationToken cancellation = default) where TResponse : class
+    public async ValueTask<TResponse> RequestAsync<TResponse>(
+        IRequest<TResponse> request,
+        IIdentity requestor,
+        CancellationToken cancellation = default) where TResponse : class
     {
         TResponse response = await new RequestDispatch(_handlerRegistry, _serviceProvider)
             .DispatchAsync(request, requestor, cancellation)
             .ConfigureAwait(false);
-        
+
         if (_options.AutoNotifyResponses)
         {
             _logger.LogInformation("Sending response of type {Response} also as notification", typeof(TResponse).Name);
-            await NotifyAsync(response, cancellation).ConfigureAwait(false);
+            await NotifyAsync(response, requestor, cancellation).ConfigureAwait(false);
         }
 
         return response;
